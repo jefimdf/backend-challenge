6daadecfe8e695dfc9346ec589b58dc53ed9742a
Jeferson Neres Farias <t991637641@cassi.com.br>
2021-12-17 19:36:26 -0300

    Finalização do Projeto

--- MercadoEletronicoCore/MercadoEletronicoCore/Repository/IPedidoItemRepository.cs
+++ MercadoEletronicoCore/MercadoEletronicoCore/Repository/IPedidoItemRepository.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MercadoEletronicoCore.Services
+namespace MercadoEletronicoCore.Repository
 {
     public interface IPedidoItemRepository
     {
